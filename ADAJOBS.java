08da5b42a5b3481256bb4eb7548f7d6b413a230a
Clayton Warstler <claywarstler@gmail.com>
2024-03-10 14:42:05 -0500

    almost done

--- ADAJOBS.java
+++ ADAJOBS.java
@@ -1,5 +1,37 @@
+import java.util.*;
+
 public class ADAJOBS {
     public static void main(String[] args) {
         // Your code here
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Boolean> todoList = new HashMap<>();
+        int queries = scanner.nextInt();
+
+        for(int i = 0; i < queries; i++) {
+            Integer value = scanner.nextInt();
+            String name = scanner.next();
+
+            if(value == 0) {
+                todoList.put(name, true);
+            } else{
+                boolean exists = checkSubstring(todoList, name);
+                if(exists) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }                
+        }
+
+        scanner.close();
+    }
+
+    public static boolean checkSubstring(Map<String, Boolean> todoList, String query) {
+        for(String job : todoList.keySet()) {
+            if(job.contains(query) || query.contains(job)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
